e0b989a52d554f6b27b034e8383f816400a9f351
TesseraSkye <39166259+TesseraSkye@users.noreply.github.com>
2021-04-14 02:05:33 -0700

    Added disclaimer, some styling

--- sprofiler/src/views/Dashboard.vue
+++ sprofiler/src/views/Dashboard.vue
@@ -5,6 +5,26 @@
         <h1 class="text-center">'Sprofiler</h1>
       </v-col>
     </v-row>
+    <v-card class="ma-2" dark>
+      <v-card-text>
+        <h1>I'm so sorry</h1>
+      </v-card-text>
+    </v-card>
+    <v-card class="ma-2" dark>
+      <v-card-text>
+        <h1>I built this in like</h1>
+      </v-card-text>
+    </v-card>
+    <v-card class="ma-2" dark>
+      <v-card-text>
+        <h1>20hrs</h1>
+      </v-card-text>
+    </v-card>
+    <v-card class="ma-2" dark>
+      <v-card-text>
+        <h1>over the course of a week</h1>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
