fe70866629a4f9b6fce4285d05a1b15e56ffd82c
TesseraSkye <39166259+TesseraSkye@users.noreply.github.com>
2021-04-14 13:16:55 -0700

    more styling

--- sprofiler/src/views/Settings.vue
+++ sprofiler/src/views/Settings.vue
@@ -1,21 +1,28 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row align-content="center">
       <v-col class="text-center" cols=12>
         <h1>Settings</h1>
       </v-col>
-      <v-col>
-        <v-card>
+      <v-col sm="12" md="6">
+        <v-card outlined elevation="10">
           <v-card-text>
             <h2>Accent Color:</h2>
           </v-card-text>
-          <v-btn color="cyan" icon @click="setAccent('cyan')">
-              <v-icon>mdi-circle</v-icon>
-          </v-btn>
-          <v-btn color="red" icon @click="setAccent('red')">
-              <v-icon>mdi-circle</v-icon>
-          </v-btn>
-
+          <v-row>
+            <v-col align-self="center">
+              <v-btn color="cyan" :small="isAccent('cyan')" fab x-small class = "mx-2 my-4" @click="setAccent('cyan')"/>
+            </v-col>
+            <v-col align-self="center">
+              <v-btn color="red" :small="isAccent('red')" fab x-small class = "mx-2 my-4" @click="setAccent('red')"/>
+            </v-col>
+            <v-col align-self="center">
+              <v-btn color="green" :small="isAccent('green')" fab x-small class = "mx-2 my-4" @click="setAccent('green')"/>
+            </v-col>
+            <v-col align-self="center">
+              <v-btn color="white" :small="isAccent('white')" fab x-small class = "mx-2 my-4" @click="setAccent('white')"/>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
@@ -31,7 +38,13 @@ export default {
   methods: {
     setAccent (data) {
       this.$store.dispatch('setAccent', data)
+    },
+    isAccent (color) {
+      return this.$store.state.accent == color
     }
+  },
+  computed: {
+    
   }
 }
 </script>
