2b2fe2e44d274fe2fc33676169fa6ecff5471510
TesseraSkye <39166259+TesseraSkye@users.noreply.github.com>
2021-05-04 17:04:36 -0700

    ble!

--- sprofiler/src/components/CardList.vue
+++ sprofiler/src/components/CardList.vue
@@ -1,20 +1,20 @@
 <template>
-    <component :is="cardComponent" :data="getStateData(this.listDataLink)"/>
-    <!-- <h2>aaa</h2> -->
+    <!-- <component :is="cardComponent" :data="getStateData(this.listDataLink)"/> -->
+    <h2>aaa</h2>
 </template>
 
 <script>
 
-function loadCard (card) {
-  return {
-    [card]: () => import(`./ShotCard.vue`)
-  }
-}
+// function loadCard (card) {
+//   return {
+//     [card]: () => import(`./ShotCard.vue`)
+//   }
+// }
 
 export default {
   props: ['listDataLink', 'cardName'],
   componets: {
-    'card-component': loadCard(this.cardName)
+    // 'card-component': loadCard(this.cardName)
   },
   methods: {
     getStateData (data) {
