030a63b7df788b0abaf31c3fc2d4bfae1df24d6c
TesseraSkye <39166259+TesseraSkye@users.noreply.github.com>
2021-08-04 22:16:59 -0700

    Overlayyyy

--- sprofiler/src/components/CoffeeCard.vue
+++ sprofiler/src/components/CoffeeCard.vue
@@ -1,23 +1,24 @@
 <template>
   <v-row>
     <v-col>
-      <v-card shaped elevation="10" color="#262626">
+      <v-card @click="this.toggleOverlay" shaped elevation="10" color="#262626">
         <v-card-title><h3>{{this.data.name}}</h3></v-card-title>
         <v-divider class="mb-2" />
-        <v-chip :key="i" v-for="i in this.data.tastingTags" class="ml-4 my-2" :color="accent" >{{i}}</v-chip>
+        <v-chip :class="[overlay ? 'blur' : '']" :key="i" v-for="i in this.data.tastingTags" class="ml-4 my-2" :color="accent" >{{i}}</v-chip>
         <v-rating
-        v-if="this.data.rating"
+          :class="[overlay ? 'blur' : '']"
+          v-if="this.data.rating"
           class="ml-4"
-            :color="this.accent"
-            dense
-            background-color="#aaa"
-            half-increments
-            readonly
-            hover
-            length="5"
-            :value="this.data.rating"
+          :color="this.accent"
+          dense
+          background-color="#aaa"
+          half-increments
+          readonly
+          hover
+          length="5"
+          :value="this.data.rating"
           />
-        <v-card-text>
+        <v-card-text :class="[overlay ? 'blur' : '']">
           <h4>
             {{this.data.process ? this.data.process + ' process' : ''}}
             {{this.data.varietals ? multiFormat(this.data.varietals) : ''}}
@@ -27,6 +28,45 @@
           <br>
           <h4>Roasted by {{this.data.roaster}} {{(this.data.roastDate ? (' on ' + this.data.roastDate) : '')}}</h4>
         </v-card-text>
+        <v-fade-transition>
+          <v-overlay
+            class="zh-90"
+            v-if="this.overlay"
+            absolute
+            opacity=0.4
+            :color="`hsl(${this.accentRaw[0]}, ${this.accentRaw[1] * 0.3}%, ${this.accentRaw[2] * 0.5}%)`">
+            <v-card-text>
+              <h4>{{this.data.brewingNotes}}</h4>
+            </v-card-text>
+            <br>
+            <br>
+            <v-btn @click="setDialog(true)" class="mx-2" block color="red">Remove</v-btn>
+          </v-overlay>
+        </v-fade-transition>
+        <v-dialog v-model="dialog" width="500">
+          <v-card>
+            <v-card-title class="headline red">
+              Erase Coffee?
+            </v-card-title>
+
+            <v-card-text class="my-2">
+              Are you sure you want to do this? This action can not be undone.
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                @click="remove()"
+              >
+                Sure
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-card>
     </v-col>
   </v-row>
@@ -44,14 +84,22 @@
 //   dateOBJ: {},
 //   uuid: '',
 //   brewingNotes: '',
-//   tastingNotes: '',
 //   tastingTags: []
 // }
 export default {
   props: ['data'],
+  data () {
+    return {
+      overlay: false,
+      dialog: false
+    }
+  },
   computed: {
     accent () {
       return this.$store.state.accent
+    },
+    accentRaw () {
+      return this.$store.state.accentRaw
     }
   },
   methods: {
@@ -66,7 +114,23 @@ export default {
         str += ' '
       }
       return str
+    },
+    remove () {
+      this.dialog = false
+      this.$store.dispatch('removeData', ['coffeeHistory', [this.data.uuid]])
+      setTimeout(() => { this.$router.push('/') }, 200)
+    },
+    setDialog (bool) {
+      this.dialog = bool
+    },
+    toggleOverlay () {
+      if (this.overlay === true) { this.overlay = false } else { this.overlay = true }
     }
   }
 }
 </script>
+<style>
+.blur {
+  filter: blur(2px);
+}
+</style>
--- sprofiler/src/views/Library.vue
+++ sprofiler/src/views/Library.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row class="pt-4">
       <v-col>
-        <v-tabs v-model="page" :color="this.getAccent" centered>
+        <v-tabs v-model="page" :color="this.accent" centered>
       <v-tab>Shots</v-tab>
       <v-tab>Coffees</v-tab>
     </v-tabs>
@@ -11,7 +11,7 @@
     <v-slide-x-transition leave-absolute hide-on-leave mode="out-in">
       <v-row v-if="page === 1" class="px-4">
         <v-col>
-          <v-btn to="/add-coffee" elevation="10" block :color="this.getAccent">
+          <v-btn to="/add-coffee" elevation="10" block :color="this.accent">
             <v-icon>mdi-plus</v-icon>
           </v-btn>
         </v-col>
@@ -22,11 +22,13 @@
         <v-tabs-items v-model="page" class="px-4">
           <v-tab-item class="pb-4">
             <!-- Shots -->
-            <shot-card class="my-4" :data="shot" :key="shot.uuid" v-for="shot in this.getStateData('shotHistory')"/>
+            <shot-card class="my-4" :data="getStateData('shotHistory')[shot]" :key="shot" v-for="shot in this.sortedShots"/>
+            <v-btn block :color="this.accent" v-if="!this.sortedShots[0] && !this.hasActiveDevices" to="/settings">Connect a device</v-btn>
+            <v-btn block :color="this.accent" v-if="!this.sortedShots[0] && this.hasActiveDevices" to="/live">Pull a Shot</v-btn>
           </v-tab-item>
           <v-tab-item>
             <!-- Coffee -->
-            <coffee-card class="my-4" :data="coffee" :key="coffee.uuid" v-for="coffee in this.getStateData('coffeeHistory')" />
+            <coffee-card class="my-4" :data="getStateData('coffeeHistory')[shot]" :key="shot" v-for="shot in this.sortedCoffees"/>
           </v-tab-item>
         </v-tabs-items>
       </v-col>
@@ -57,11 +59,21 @@ export default {
     })
   },
   computed: {
-    getAccent () {
+    accent () {
       return this.$store.state.accent
     },
     routePage () {
       return 1
+    },
+    sortedShots () {
+      return Object.keys(this.getStateData('shotHistory')).sort((a, b) => b - a)
+    },
+    sortedCoffees () {
+      return Object.keys(this.getStateData('coffeeHistory')).sort((a, b) => b - a)
+    },
+    hasActiveDevices () {
+      const ad = this.$store.state.activeDevices // !! casts return as a bool
+      return (!(ad && Object.keys(ad).length === 0 && ad.constructor === Object)) // is it truthy, is 0 length, and is an object
     }
   },
   methods: {
